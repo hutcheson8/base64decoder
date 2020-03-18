1d030898b7a5d0505879e2e404baadd8a07ddb12
hutcheson8 <hutcheson8@gmail.com>
2020-03-18 02:40:37 -0500

    Make compatible with java 8

--- src/radix64decode/Main.java
+++ src/radix64decode/Main.java
@@ -4,12 +4,18 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
 import java.util.Base64;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedInputStream bis = new BufferedInputStream(new FileInputStream("r64.txt"));
-		byte[] in = bis.readAllBytes();
+		ArrayList<Byte> inAL = new ArrayList<>();
+		for(int x=bis.read();x!=-1;x=bis.read())
+			inAL.add((byte) x);
+		byte[] in = new byte[inAL.size()];
+		for(int x=0;x<in.length;x++)
+			in[x]=inAL.get(x);
 		bis.close();
 		byte[] out = Base64.getDecoder().decode(in);
 		BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("out.txt"));
